7aa76d492741cab622fe0dced3f6c26c5a038087
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-30 16:26:20 -0600

    added card ids

--- server/controllers/cardsController.js
+++ server/controllers/cardsController.js
@@ -1,5 +1,7 @@
 const cards = require('../data.json')
 
 module.exports = {
-    getAllCards: (req, res) => {}
+    getAllCards: (req, res) => {
+        res.status(200).send(cards)
+    },
 }
--- server/controllers/deckController.js
+++ server/controllers/deckController.js
@@ -4,12 +4,21 @@ const deck = {total: 0, items: []}
 let deckId = 0
 
 module.exports = {
-    getDeck : (req, res) => {},
-    addToDeck : (req, res) => {},
+    getDeck : (req, res) => {
+        res.status(200).send(deck)
+    },
+    addToDeck : (req, res) => {
+        const{cardId} = req.body 
+
+        const index = deck.items.findIndex((element) => element.id === +cardId)
+
+        if (index === -1){
+            const card = cards.find((element) => element.id === +cardId)
+            res.send(card)
+        }
+
+    },
     changeQuantity : (req, res) => {},
     removeFromDeck : (req, res) => {},
     delete : (req, res) => {}
-
-
-
 }
