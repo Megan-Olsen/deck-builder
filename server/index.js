73e8b21479c7cdedddc7e10a550aa1f013330d5a
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-30 15:16:58 -0600

    sneaky backtics

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
-
+const cardsCtrl = require('./controllers/cardsController')
+const deckCtrl = require('./controllers/deckController')
 
 const SERVER_PORT = 5000
 
@@ -8,14 +9,14 @@ app.use(express.json())
 
 
 //endpoints go here!
-app.get('/api/cards', cardsCtrl.getAllCards) 
+// app.get('/api/cards', cardsCtrl.getAllCards) 
 
 
 
-app.get('/api/deck', deckCtrl.getDeck)
-app.post('/api/deck', deckCtrl.addToDeck)
-app.put('/api/deck/:deck_id', deckCtrl.changeQuantity)
-app.delete('/api/deck/:deck_id', deckCtrl.removeFromDeck)
-app.delete('/api/deck', deckCtrl.delete)
+// app.get('/api/deck', deckCtrl.getDeck)
+// app.post('/api/deck', deckCtrl.addToDeck)
+// app.put('/api/deck/:deck_id', deckCtrl.changeQuantity)
+// app.delete('/api/deck/:deck_id', deckCtrl.removeFromDeck)
+// app.delete('/api/deck', deckCtrl.delete)
 
-app.listen(5000, () => console.log('Funky deck building on 5000'))
+app.listen(SERVER_PORT, () => console.log(`Funky deck building on port ${SERVER_PORT}`))
