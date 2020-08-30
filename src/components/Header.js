83c4c30ff7e3dae3212bbc75a66e48f9a30effab
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-30 14:08:15 -0600

    header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <div className="header">
+      <h1>Magic The Gathering</h1>
+      <h2>Deck Builder</h2>
+    </div>
+  )
+}
+export default Header
