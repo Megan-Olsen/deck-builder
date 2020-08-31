41131a847fd67bc8499bef8d16a126f99eaecb24
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-31 09:55:34 -0600

    done

--- src/components/DeckCard.js
+++ src/components/DeckCard.js
@@ -11,7 +11,7 @@ const DeckCard = (props) => {
                 <div className="button-hold">
                     <button onClick={() => props.changeQuantity(deckId, 'down')}>-</button>
                     <button onClick={() => props.changeQuantity(deckId, 'up')}>+</button>
-                    <button className="trashcan" onClick={() => props.removeFromDeck(deckId)}><img src="https://w7.pngwing.com/pngs/616/37/png-transparent-trash-can-illustration-computer-icons-icon-design-delete-button-miscellaneous-text-rectangle-thumbnail.png"/></button>
+                    <button onClick={() => props.removeFromDeck(deckId)}><img className="trashcan" src="https://w7.pngwing.com/pngs/616/37/png-transparent-trash-can-illustration-computer-icons-icon-design-delete-button-miscellaneous-text-rectangle-thumbnail.png"/></button>
                 </div>
             </div>
         </div>
