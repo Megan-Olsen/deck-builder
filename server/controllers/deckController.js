e07a98b2bc32a475d9c0cff21069bb3f5982bf7e
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-30 18:29:35 -0600

    totals

--- server/controllers/deckController.js
+++ server/controllers/deckController.js
@@ -3,11 +3,13 @@ const cards = require('../data.json')
 const deck = {total: 0, items: []}
 let deckId = 0
 
-// const updateCartTotal = () => {
-//     const total = deck.items.reduce((acc, element) => {
-//       return acc + element.quantity
-//     }, 0)
-//   }
+const updateDeckTotal = () => {
+    const total = deck.items.reduce((acc, element) => {
+        return acc + element.quantity
+    }, 0)
+
+    deck.total = total
+}
 
 module.exports = {
     getDeck : (req, res) => {
@@ -30,9 +32,33 @@ module.exports = {
         } else {
             deck.items[index].quantity += +quantity
         }
+        updateDeckTotal()
+        
         res.status(200).send(deck)
     },
-    changeQuantity : (req, res) => {},
+    changeQuantity : (req, res) => {
+        const {deck_id} = req.params
+        const {action} = req.query
+
+        const index = deck.items.findIndex((element) => element.deckId === +deck_id)
+
+        if(index === -1){
+            return res.status(404).send('Card not in Deck')
+        }
+
+        if(action === 'up'){
+            deck.items[index].quantity += 1
+        } else {
+            if(deck.items[index].quantity > 1){
+                deck.items[index].quantity -= 1
+            } else {
+                deck.items.splice(index, 1)
+            }
+        }
+        updateDeckTotal()
+        
+        res.status(200).send(deck)
+    },
     removeFromDeck : (req, res) => {},
     delete : (req, res) => {}
 }
