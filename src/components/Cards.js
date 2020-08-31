d847a17e222ad87d0d5e2cc10329fcab365928f6
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-31 12:57:26 -0600

    finsihed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Card from './Card'
 
 const Cards = (props) => {
-return <div className="cards">{props.cards.map(element => {
-    return <Card addToDeck={props.addToDeck} key={element.id} data={element} />
-})}
-</div>
+return ( 
+    <div className="cards">{props.cards.map(element => {
+        return <Card addToDeck={props.addToDeck} key={element.id} data={element} />
+    })}
+    </div>
+)
 }
 export default Cards
